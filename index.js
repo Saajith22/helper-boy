30d93e4738c0966bf308c9c10997ffba1bf778bd
Exxon <65795854+Exxonnnnnn@users.noreply.github.com>
2021-05-14 13:19:57 +0100

    Destructure
    I'm still very confused by the use of this package. I don't really see the point in downloading a whole package to make an embed or connect to MongoDB, you also haven't added all the options when connecting to MongoDB.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const discord = require('discord.js');
-const mongoose = require('mongoose');
+const { MessageEmbed } = require('discord.js');
+const { connect } = require('mongoose');
 
 async function embed(title, description, color) {
-    const e = new discord.MessageEmbed()
+    const e = new MessageEmbed()
         .setTitle(title)
         .setDescription(description)
         .setColor(color)
@@ -18,13 +18,13 @@ async function Mongodb(uri, {
     if (typeof uri !== 'string') return console.log('Invalid MongoDB uri provided! (Not a String) -- [Helper Boy]')
 
     if (modify === true) {
-        mongoose.connect(uri, {
+        connect(uri, {
             useFindAndModify: false,
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
     } else {
-        mongoose.connect(uri, {
+        connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
